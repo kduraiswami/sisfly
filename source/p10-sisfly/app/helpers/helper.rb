2801a025900bf48cba05176fecca42bbb09fc9ff
Kailash <kaidurai@gmail.com>
2015-04-02 12:04:17 -0700

    added methods for location selector and sisfly score, need to complete logic for sisfly score but location selector works

--- source/p10-sisfly/app/helpers/helper.rb
+++ source/p10-sisfly/app/helpers/helper.rb
@@ -8,34 +8,32 @@ helpers do
     @javascript_env ||= {}
   end
 
-
-  # def relevant_crime(path_coordinates)
-  #  path_coordinates.each do |individual_coordinate|
-  #     nearby_crimes(individual_coordinate)
-  #   end
-  # end
-
   def nearby_crimes(individual_coordinate)
     json_crimes = ""
     open("https://data.sfgov.org/resource/tmnf-yvry.json?$where=within_circle(location,#{individual_coordinate["k"]},#{individual_coordinate["D"]},20)") do |all_crimes|
-      all_crimes.each_line {|individual_crime| json_crimes << individual_crime}
+      all_crimes.each_line do |individual_crime|
+        json_crimes << individual_crime
+      end
     end
     hash_crimes = JSON.parse(json_crimes)
     hash_crimes.map!{|crime_args| crime_args.delete_if{|key, value| key == "location"}}
     hash_crimes
   end
+  def location_selector(crime_data)
+    #this will (for now) randomly select a crime from each result array from nearby_crimes
+    #this is a destructive method but assigned to a new variable in the controller
+    selected_crimes = []
+    crime_data.each do |coordinate_crime_array|
+      selected_crimes << coordinate_crime_array.sample
+    end
+    selected_crimes[1..-1]
+  end
 
-  # hash_crimes.each do |crime_args|
-  #   crime = Crime.new(crime_args)
-  # end
+  def sisfly_score(crime_data)
+    #this is a hash with a score based on the crimes parsed and a location for google maps to place a marker
 
-    # relevant_crime = []
+  end
 
-    # weight_crimes(relevant_crime)
-  # end
 
-  # def weight_crimes(relevant_crime)
-
-  # end
 
 end
